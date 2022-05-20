13d9e502beb7839f54fb3b1e885a6e072081eb2b
jefferson <jefferson.medina@sourcemeridian.com>
2022-05-20 16:19:39 -0500

    update data info

--- src/app/technology/technology-list/technology-list.component.ts
+++ src/app/technology/technology-list/technology-list.component.ts
@@ -7,6 +7,14 @@ import { selectAllJobs } from 'src/app/jobs/store/reducers/jobs.reducer';
 import { Technology } from '../class/technology';
 import { selectAllTechnologies, selectedTecnologisWithJobs } from '../store/reducers/technology.reducer';
 
+class JobsAndTechs implements Jobs, Technology {
+  readonly id: number = 0;
+  readonly name_job:string = '';
+  readonly id_technology:number = 0
+  readonly id_tech: number = 0;
+  readonly name_technology: string = '';
+}
+
 @Component({
   selector: 'app-technology-list',
   templateUrl: './technology-list.component.html',
@@ -16,14 +24,14 @@ export class TechnologyListComponent implements OnInit {
 
   technologies : Observable<Technology[]> =  of<Technology[]>()
   jobs : Observable<Jobs[]> =  of<Jobs[]>()
-  jobsAndTechs : Observable<any[]> =  of<any[]>()
+  jobsAndTechs : Observable<JobsAndTechs[]> =  of<JobsAndTechs[]>()
 
   constructor(private store :Store<AppState>){ }
 
   ngOnInit(): void {
     this.technologies =this.store.select(selectAllTechnologies);
     this.jobs = this.store.select(selectAllJobs);
-    this.jobsAndTechs =  this.store.select(selectedTecnologisWithJobs);    
+    this.jobsAndTechs =  this.store.select(selectedTecnologisWithJobs);
 
   }
 
--- src/app/technology/utils/functions.ts
+++ src/app/technology/utils/functions.ts
@@ -2,10 +2,16 @@ import { Jobs } from "src/app/jobs/class/jobs";
 import { Technology } from "../class/technology";
 
 export const joinJobsAndTecnologies = (jobs: Jobs[], technologies:Technology[]) =>{
-    return jobs.map((job) => {
+    return jobs.map((job: Jobs) => {
         return technologies
             .filter((technology:Technology) =>  technology.id === job.id_technology)
-            .map((tech)=> ({...job, ...tech} ))                        
+            .map((tech: Technology)=> ({
+                id: job.id,
+                name_job: job.name_job,
+                id_technology: job.id_technology,
+                id_tech: tech.id,
+                name_technology: tech.name_technology
+            }))
     })
     .reduce((a,b)=>{
         return a.concat(b)
